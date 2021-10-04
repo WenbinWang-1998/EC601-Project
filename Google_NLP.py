2b30fcfdb71c90b1623e173c0a115bb0f5ed0583
WenbinWang-1998 <74896960+WenbinWang-1998@users.noreply.github.com>
2021-10-03 21:04:29 -0400

    Project2_p2
    Using Google Cloud Platform to write read sentence sentiment and get entities.

---
+++ Google_NLP.py
@@ -0,0 +1,61 @@
+from google.cloud import language
+
+#
+# def analyze_text_sentiment(text):
+#     client = language.LanguageServiceClient()
+#     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
+#
+#     response = client.analyze_sentiment(document=document)
+#
+#     sentiment = response.document_sentiment
+#     results = dict(
+#          text=text,
+#          score=f"{sentiment.score:.1%}",
+#          magnitude=f"{sentiment.magnitude:.1%}",
+#      )
+#     for k, v in results.items():
+#          print(f"{k:10}: {v}")
+#
+#
+# def analyze_text(doc):
+#     # client = language.LanguageServiceClient()
+#     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
+#
+#     response = client.analyze_sentiment(document=document)
+#
+#     sentiment = response.document_sentiment
+#     results = dict(
+#         text=text,
+#         score=f"{sentiment.score:.1%}",
+#         magnitude=f"{sentiment.magnitude:.1%}",
+#     )
+#     for k, v in results.items():
+#         print(f"{k:10}: {v}")
+
+
+
+def entities_sentiment(doc):
+    client = language.LanguageServiceClient()
+    document = language.Document(content=doc, type_=language.Document.Type.PLAIN_TEXT)
+    response = client.analyze_entity_sentiment(document=document,encoding_type='UTF32')
+    res=client.analyze_sentiment(document=document,encoding_type='UTF32',)
+    sentiment = res.document_sentiment
+    fin = dict(
+        sentence=doc,
+        overallscore=f"{sentiment.score:.1%}",
+        overallmagnitude=f"{sentiment.magnitude:.1%}",
+    )
+    print("overall sentiment: ")
+    for key, value in fin.items():
+        print(f"{key:15}: {value}")
+    for entity in response.entities:
+        print("-------------------------------------")
+        results = dict(
+            name=entity.name,
+            type=entity.type_.name,
+            salience=f"{entity.salience:.1%}",
+            score=entity.sentiment.score,
+            magnitude=entity.sentiment.magnitude
+        )
+        for key, value in results.items():
+            print(f"{key:15}:  {value}")
