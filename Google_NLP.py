90375ff14b6a3cfdce19651aae88697611c6b455
WenbinWang-1998 <74896960+WenbinWang-1998@users.noreply.github.com>
2021-10-03 21:04:54 -0400

    Update Google_NLP.py

--- Google_NLP.py
+++ Google_NLP.py
@@ -1,38 +1,5 @@
 from google.cloud import language
 
-#
-# def analyze_text_sentiment(text):
-#     client = language.LanguageServiceClient()
-#     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
-#
-#     response = client.analyze_sentiment(document=document)
-#
-#     sentiment = response.document_sentiment
-#     results = dict(
-#          text=text,
-#          score=f"{sentiment.score:.1%}",
-#          magnitude=f"{sentiment.magnitude:.1%}",
-#      )
-#     for k, v in results.items():
-#          print(f"{k:10}: {v}")
-#
-#
-# def analyze_text(doc):
-#     # client = language.LanguageServiceClient()
-#     document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
-#
-#     response = client.analyze_sentiment(document=document)
-#
-#     sentiment = response.document_sentiment
-#     results = dict(
-#         text=text,
-#         score=f"{sentiment.score:.1%}",
-#         magnitude=f"{sentiment.magnitude:.1%}",
-#     )
-#     for k, v in results.items():
-#         print(f"{k:10}: {v}")
-
-
 
 def entities_sentiment(doc):
     client = language.LanguageServiceClient()
